570cd26f12750d22634c9c65c2eed6890fac6a1b
Oleg Granevskij <o.granevskij@gmail.com>
2024-04-04 05:44:06 +0300

    remote1 config

---
+++ ng16-remote1/src/app/fruits/fruits-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FruitsComponent } from './fruits/fruits.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FruitsComponent,
+    pathMatch: 'full',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FruitsRoutingModule { }
--- ng16-remote1/webpack.config.js
+++ ng16-remote1/webpack.config.js
@@ -1,15 +1,69 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-
+const config = withModuleFederationPlugin({
   name: 'ng16-remote1',
-
   exposes: {
-    './Component': './src/app/app.component.ts',
+    './Module': './src/app/fruits/fruits.module.ts',
+    './Component': './src/app/fruits/fruits/fruits.component.ts',
   },
-
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
-
 });
+
+const newConfig = {
+  ...config,
+  output: {
+    ...config.output,
+    scriptType: 'text/javascript',
+    publicPath: 'http://localhost:4201/',
+  },
+};
+
+module.exports = newConfig;
+
+// const { shareAll, share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+// const config = withModuleFederationPlugin({
+//   name: 'mfe1',
+//   filename: 'remoteEntry.js',
+//   exposes: {
+//     './Component': './src/app/app.component.ts',
+//   },
+//   shared:
+//   share({
+//     '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+//     '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+//     '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+//     '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+//   }),
+//   // {
+//   //   ...shareAll({
+//   //     singleton: true,
+//   //     strictVersion: true,
+//   //     requiredVersion: 'auto',
+//   //   }),
+//   // },
+// });
+
+// console.log('conf', config);
+
+// module.exports = {
+//   ...config,
+//   output: {
+//     ...config.output,
+//     uniqueName: 'mfe1',
+//     // filename: '[name].[contenthash].js',
+//     scriptType: 'text/javascript',
+//     publicPath: 'http://localhost:4201/',
+//   },
+//   // experiments: {
+//   //   ...config.experiments,
+//   //   // topLevelAwait: true,
+//   // },
+//   optimization: {
+//     runtimeChunk: false,
+//     splitChunks: false,
+//     minimize: true,
+//   },
+// };
